cc25d018e2da74459764297c1e2b907578aea491
Maha Abdul <abdul_@auca.kg>
2022-09-14 15:06:01 +0600

    Problem09 operation pi is completed

---
+++ lab-01/src/Problem10.java
@@ -0,0 +1,13 @@
+/**  (Population projection) The U.S. Census Bureau projects population based on
+ the following assumptions:
+ ■ One birth every 7 seconds
+ ■ One death every 13 seconds
+ ■ One new immigrant every 45 seconds
+ Write a program to display the population for each of the next five years. Assume
+ that the current population is 312,032,486, and one year has 365 days.*/
+
+public class Problem10 {
+    public static void main(String[] args) {
+
+    }
+}
