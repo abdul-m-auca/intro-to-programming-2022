b8cfa17db76652925f40af51f7408c74a03aaaaa
Maha Abdul <abdul_@auca.kg>
2022-09-14 14:10:31 +0600

    Problem06 pattern Java is completed

--- lab-01/src/Problem06.java
+++ lab-01/src/Problem06.java
@@ -1,4 +1,4 @@
-public class problem06 {
+public class Problem06 {
     public static void main(String[] args) {
         System.out.println("   J    a    v     v    a  ");
         System.out.println("   J   a a    v   v    a a");
