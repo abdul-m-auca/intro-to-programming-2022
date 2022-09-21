12a20ea846c97dfcc2b9c6454fc3ba721abdbdfb
Maha Abdul <abdul_@auca.kg>
2022-09-21 12:50:22 +0600

    Problem05 is completed

--- lab-01/src/Problem05.java
+++ lab-01/src/Problem05.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
 public class Problem05 {
+    public static void main(String[] args) {
+        // we need it to read two integer values
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1st double? ");
+        // int is a type, "a" is a variable of type int
+        double a = input.nextDouble();
+
+        System.out.print("2st double? ");
+        double b = input.nextDouble();
+        double sum = a + b;
+        double sub = a - b;
+        double mul = a * b;
+        double div = a / b;
+        double rem = a % b;
+
+        System.out.println(a + " + " +b + " = " + sum);
+        System.out.println(a + " - " +b + " = " + sub);
+        System.out.println(a + " * " +b + " = " + mul);
+        System.out.println(a + " / " +b + " = " + div);
+        System.out.println(a + " % " +b + " = " + rem);
+
+    }
 }
