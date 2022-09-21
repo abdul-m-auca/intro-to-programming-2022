e19b3750f8314fcf81f96884304681cea886e8f5
Maha Abdul <abdul_@auca.kg>
2022-09-21 14:31:43 +0600

    Problem06 of lab-02 is completed.

---
+++ lab-02/src/Problem06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Problem06 {
+    public static void main(String[] args) {
+        //Create new Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // prompt to enter double input
+        System.out.print("Enter a degree in celsius: ");
+        double Celsius = input.nextDouble();
+
+        // convert celsius to Fahrenheit
+
+        double Fahrenheit =9.0/5 *Celsius +32;
+        // Display results
+        System.out.println(Celsius + " celsius is " + Fahrenheit + " Fahrenheit is ");
+
+    }
+}
