ed54ca9b69efc724acfafa2dcf254c46bbc74c67
Maha Abdul <abdul_@auca.kg>
2022-09-28 15:21:24 +0600

    Problem01 is solved..

--- oj-midterm/src/Problem01.java
+++ oj-midterm/src/Problem01.java
@@ -3,6 +3,6 @@ public class Problem01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String word = input.nextLine();
-        System.out.println(word+ " " +word + " " + word);
+        System.out.println(word+ " " + word + " " + word);
     }
 }
