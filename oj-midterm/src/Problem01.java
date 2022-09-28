c92db0c262e9d5fdb6d27cba0d44c7c38cd325b7
Maha Abdul <abdul_@auca.kg>
2022-09-28 15:00:42 +0600

    space error

---
+++ oj-midterm/src/Problem01.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        System.out.println(word+ "" +word + " " + word);
+    }
+}
