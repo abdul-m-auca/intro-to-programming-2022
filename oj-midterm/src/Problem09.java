041044f617325982bdfeaddd69177de0e8f2eab5
Maha Abdul <abdul_@auca.kg>
2022-09-28 15:11:15 +0600

    Problem09 is almost completed

---
+++ oj-midterm/src/Problem09.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Problem09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double r = input.nextDouble();
+        double h = input.nextDouble();
+
+        double v = PI
+
+    }
+}
