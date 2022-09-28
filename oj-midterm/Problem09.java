e9656672041dddf53e720c0a2d44587bc05fae2d
Maha Abdul <abdul_@auca.kg>
2022-09-28 14:32:45 +0600

    usernames

---
+++ oj-midterm/Problem09.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Problem09 {
+    public static void main(String[] args) {
+        Scanner import = new Scanner(System.in);
+
+        double r = input.nextDouble();
+        double h = input.nextDouble();
+
+        double v = Math.PI * r * r *h / 3;
+
+        System.out.printf("%.2fn", v);
+    }
+
+}
