a80fffe111f6a5f52c50ae88389eee344d8cfa7c
Maha Abdul <abdul_@auca.kg>
2022-09-30 15:38:32 +0600

    Problem05 is almost completed

---
+++ oj-midterm/src/Problem05.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int R = input.nextInt();
+        int S = input.nextInt();
+
+        int L = (R*8+S*3)-28;
+
+        System.out.println(L);
+    }
+}
