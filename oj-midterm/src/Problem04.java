fe1b1537a5062bc3265356671ffec136feac1a9c
Maha Abdul <abdul_@auca.kg>
2022-09-30 14:41:54 +0600

    working on problem04 again

--- oj-midterm/src/Problem04.java
+++ oj-midterm/src/Problem04.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class Problem04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int h = input.nextInt();
-        int b = input.nextInt();
+        float h = input.nextInt();
+        float b = input.nextInt();
 
-        int a = (b*h)/2;
+       float a = (b*h)/2;
 
-        System.out.printf("%.1f%n",a);
+        System.out.println(a);
 
     }
 }
