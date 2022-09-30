314c0805767cbb63a4292272ff0befe9ae222072
Maha Abdul <abdul_@auca.kg>
2022-09-30 16:31:33 +0600

    Problem03 is completed with correct solution..

---
+++ lab-02/src/Problem03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("A four digit integer? ");
+        int sum = input.nextInt();
+
+        System.out.println("A four digit integer? "+ sum);
+
+    }
+}
