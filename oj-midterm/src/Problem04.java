12c3e28c13bb6f4df2e213dc077d4b20a92e4827
Maha Abdul <abdul_@auca.kg>
2022-09-30 14:40:19 +0600

    working on problem04

---
+++ oj-midterm/src/Problem04.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Problem04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int h = input.nextInt();
+        int b = input.nextInt();
+
+        int a = (b*h)/2;
+
+        System.out.printf("%.1f%n",a);
+
+    }
+}
