40b0241507d7a663fb610decff8ecfdf1a08ee52
Maha Abdul <abdul_@auca.kg>
2022-09-30 16:13:47 +0600

    Problem05 is completed with correct solution.

---
+++ lab-02/src/Problem02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("1st value? ");
+        int a = input.nextInt();
+
+        System.out.print("2nd value? ");
+        int b = input.nextInt();
+
+        System.out.printf("Before swapping: a = %d, b = %d;%n",a, b);
+        System.out.printf("After swapping: a = %d, b = %d;%n",b, a);
+    }
+}
