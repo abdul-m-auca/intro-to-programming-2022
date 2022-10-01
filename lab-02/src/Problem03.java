201f8a3a880c1df9a93eb7d481b888096b166d1b
Maha Abdul <abdul_@auca.kg>
2022-10-01 19:52:47 +0600

    Problem03 is completed with correct solution..

--- lab-02/src/Problem03.java
+++ lab-02/src/Problem03.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class Problem03 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("A four digit integer? ");
-        int number = input.nextInt();
 
-        int sum =0;
-        while (number!=0) {
-            sum = sum +number %10;
-            number = number /10;
-            System.out.println("the sum of all digits"+ sum);
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("A four-digit integer? ");
+        int digits = in.nextInt();
+        System.out.println("The sum of all digits is " + sumDigits(digits));
+    }
+    public static int sumDigits(long n) {
+        int result = 0;
+
+        while(n > 0) {
+            result += n % 10;
+            n /= 10;
         }
 
+        return result;
     }
+
 }
