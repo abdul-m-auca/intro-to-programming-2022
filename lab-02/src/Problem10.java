9b9266b0261499ca2da0f9b69999c843abad9cd2
Maha Abdul <abdul_@auca.kg>
2022-10-02 16:13:17 +0600

    Problem10 is completed with correct solution..

--- lab-02/src/Problem10.java
+++ lab-02/src/Problem10.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class Problem10 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of years: ");
+        int years = input.nextInt();
+
+        int population = 312032486 + (((31536000 / 7) - (31536000 / 13)
+                + (31536000 / 45)) * years);
+        System.out.println("The population in " + years + "years is " + population);
+
+
 
     }
 }
