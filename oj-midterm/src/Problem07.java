e9901539b2a365b231381be01c50dbea59bd41af
Maha Abdul <abdul_@auca.kg>
2022-10-03 13:35:11 +0600

    Problem07 is completed with correct solution.

---
+++ oj-midterm/src/Problem07.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Problem07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int R1 = input.nextInt();
+
+        int S = input.nextInt();
+
+        int R2 = 2*S - R1;
+
+        System.out.println(R2);
+    }
+}
