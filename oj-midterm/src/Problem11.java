75745193ed08ae87d2264e565919f718abcfa88e
Maha Abdul <abdul_@auca.kg>
2022-10-05 13:21:07 +0600

    Problem11 is completed with correct solution.

---
+++ oj-midterm/src/Problem11.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Problem11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+
+        int I = input.nextInt();
+
+
+        System.out.println((A*I)-(A-1));
+
+    }
+}
