fb9802d089c0327bfda73486274eb7d942026489
Maha Abdul <abdul_@auca.kg>
2022-10-11 02:39:47 +0600

    Problem03 is accepted with correct solution.

--- oj-midterm/src/Problem03.java
+++ oj-midterm/src/Problem03.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Problem03 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         int youngest = in.nextInt(),
                 middle = in.nextInt(),
                 diff = middle - youngest,
