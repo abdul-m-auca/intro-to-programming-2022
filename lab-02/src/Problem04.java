03dcd66f36d0155eeee0212906a94a880cfb3448
Maha Abdul <abdul_@auca.kg>
2022-10-12 13:21:31 +0600

    Problem04 is changed

--- lab-02/src/Problem04.java
+++ lab-02/src/Problem04.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 public class Problem04 {
     public static final double CM_PER_INCH = 2.54;
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Length in inches? ");
 
-        double inches = input.nextInt();
+        double inches = input.nextDouble();
         double lengthInCm = CM_PER_INCH * 2.54;
 
         //results
 
-        System.out.println("%.2f in. = %.2f cm%n", inches, lengthInCm);
+        System.out.printf("%.2f in. = %.2f cm%n", inches, lengthInCm);
     }
 }
