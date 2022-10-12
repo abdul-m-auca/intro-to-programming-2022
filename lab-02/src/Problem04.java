24839b04bbe95162d90241d6a6ce8df63eb89efe
Maha Abdul <abdul_@auca.kg>
2022-10-12 13:23:37 +0600

    Problem04 is updated with new solution.

--- lab-02/src/Problem04.java
+++ lab-02/src/Problem04.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Problem04 {
     public static final double CM_PER_INCH = 2.54;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -12,6 +13,6 @@ public class Problem04 {
 
         //results
 
-        System.out.printf("%.2f in. = %.2f cm%n.", inches, lengthInCm);
+        System.out.printf("%.2f in. = %.2f cm%n", inches, lengthInCm);
     }
 }
