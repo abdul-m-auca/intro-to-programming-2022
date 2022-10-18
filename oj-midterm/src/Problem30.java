c6d6e61eb06b96b0403384b7c1c9fc73b66cf410
Maha Abdul <abdul_@auca.kg>
2022-10-18 23:46:25 +0600

    Problem30 is accepted with updated solution.

--- oj-midterm/src/Problem30.java
+++ oj-midterm/src/Problem30.java
@@ -12,15 +12,15 @@ public class Problem30 {
         int sum = angle1 + angle2 + angle3;
 
         if (sum > 180) {
-            System.out.println("error");
+            System.out.println("Error");
         } else if (sum < 180) {
-            System.out.println("error");
+            System.out.println("Error");
         } else if (angle1 == 60 && angle2 == 60 && angle3 == 60) {
-            System.out.println(" equilateral");
+            System.out.println("Equilateral");
         } else if (angle1 == angle2 || angle2 == angle3 || angle3 == angle1) {
-            System.out.println("isosceles");
+            System.out.println("Isosceles");
         } else {
-            System.out.println("scalene");
+            System.out.println("Scalene");
         }
     }
 }
