fd46fc9ebdb4cf482e8350df1334464e23c7392b
Maha Abdul <abdul_@auca.kg>
2022-10-18 16:54:24 +0600

    Problem01 is working on it.

--- lab-03/src/Problem01.java
+++ lab-03/src/Problem01.java
@@ -1,7 +1,18 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Problem01 {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("some of real number");
+
+        double x = input.nextDouble();
+        double r = x;
+
+        if
     }
 }
