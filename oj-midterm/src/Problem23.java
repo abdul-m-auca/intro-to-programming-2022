78f29a171af1d25a63d96a48fe84db60e30a7e30
Maha Abdul <abdul_@auca.kg>
2022-10-19 13:43:28 +0600

    Problem23 is not accepted.

---
+++ oj-midterm/src/Problem23.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Problem23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        if (a == 0) {
+            a = a + 24;
+        }
+
+        if (b < 45) {
+            a = a - 1;
+        }
+        if (b < 45) {
+            b = b + 60;
+        }
+        int time = b - 45;
+
+        System.out.print(a + " ");
+
+        System.out.print(time);
+    }
+}
