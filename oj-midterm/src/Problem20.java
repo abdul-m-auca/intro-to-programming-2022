0973ce029702ecfd61c1cdaa41d2f0ac3377b5bc
Maha Abdul <abdul_@auca.kg>
2022-10-19 13:37:02 +0600

    Problem20 was not accepted.

---
+++ oj-midterm/src/Problem20.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Problem20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+
+        int result = (number = 5550000) && (number < 5560000) ? 1 : 0;
+        System.out.print(result);
+    }
+}
