6457faaa8434d1be7527bcdaab70f33226d59f1e
Maha Abdul <abdul_@auca.kg>
2022-10-24 13:40:27 +0600

    Problem31 is almost completed.

--- oj-midterm/src/Problem31.java
+++ oj-midterm/src/Problem31.java
@@ -8,11 +8,15 @@ public class Problem31 {
         int month = input.nextInt();
         ///day input
         int day = input.nextInt();
-
+        //in january or before february 18
         if (month < 2 || (month == 2 && day < 18)) {
-            System.out.print("Before");
-        } else if (month > 2 || (month == 2 && day > 18)) {
-            System.out.print("After");
+            System.out.println("Before");
+        }
+        //March-December or after February
+        else if (month > 2 || (month == 2 && day > 18)) {
+            System.out.println("After");
+        } else {
+            System.out.println("Special");
         }
 
     }
