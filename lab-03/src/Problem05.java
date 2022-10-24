309d84bac59d004ff7dcac689d086fb69b822dfc
Maha Abdul <abdul_@auca.kg>
2022-10-24 14:52:00 +0600

    Problem01 and problem05 is almost completed.

---
+++ lab-03/src/Problem05.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Points? ");
+
+        double points = input.nextDouble();
+
+        if (90 <= points && points <=100){
+            System.out.println("Grade: A");
+        }
+        if (90 <= points && points <=100){
+            System.out.println("Grade: A");
+
+            if (90 <= points && points <=100){
+            System.out.println("Grade: A");
+        }
+            if (90 <= points && points <=100){
+                System.out.println("Grade: A");
+        }if (90 <= points && points <=100){
+            System.out.println("Grade: A");
+        }
+    }
+}
---
+++ lab-04/src/Problem01.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double x = 0.1;
+
+        double s = 0;
+
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+        s += x;
+
+        //if (s==1.0)
+        if (Math.abs(s- 1) <= 1E-9) {
+            System.out.println("Ok");
+        } else {
+            System.out.println("What?!");
+            System.out.println(s);
+        }
+    }
+}
