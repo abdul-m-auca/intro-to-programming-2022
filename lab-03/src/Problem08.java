4469d8e7a606c88fc799e4efd17c737e733670db
Maha Abdul <abdul_@auca.kg>
2022-10-25 22:19:21 +0600

    Problem08 is completed with valid solution.

--- lab-03/src/Problem08.java
+++ lab-03/src/Problem08.java
@@ -5,24 +5,26 @@ public class Problem08 {
         Scanner input = new Scanner(System.in);
 
         // Prompt the user to enter three integers
+        System.out.print("Enter three integer numbers: ");
         int number1 = input.nextInt();
         int number2 = input.nextInt();
         int number3 = input.nextInt();
 
 
+        int t;
         // Sort numbers
         if (number2 < number1) {
-            int t = number1;
+            t = number1;
             number1 = number2;
             number2 = t;
         }
-        if (number2 < number3) {
-            int t = number2;
-            number2 = number3;
+        if (number3 < number1) {
+            t = number3;
+            number1 = number3;
             number3 = t;
         }
         if (number3 < number2) {
-            int t = number2;
+            t = number2;
             number2 = number3;
             number3 = t;
         }
