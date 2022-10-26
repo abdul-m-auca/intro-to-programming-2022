fe503ecaafbb2e7b4513051ca558f5f2e1d04c26
Maha Abdul <abdul_@auca.kg>
2022-10-26 13:45:26 +0600

    Problem04 is completed.

--- lab-04/src/Problem04.java
+++ lab-04/src/Problem04.java
@@ -4,6 +4,27 @@ public class Problem04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("complexity level? ");
+        int level = input.nextInt();
 
+        switch (level) {
+            case 4:
+            case 5:
+                System.out.println("You are a pro gamer ");
+                break;
+            case 2:
+            case 3:
+                System.out.println("You are an experienced gamer ");
+                break;
+            case 1:
+                System.out.println("You are a beginner ");
+                break;
+            case 0:
+                System.out.println("You are a total newbie ");
+                break;
+            default:
+                System.out.println("Incorrect level number" + level);
+                break;
+        }
     }
 }
