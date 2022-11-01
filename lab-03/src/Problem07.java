1565c79312b2f5df379bd2e0d3ac63bfcffc63e9
Maha Abdul <abdul_@auca.kg>
2022-11-02 00:38:37 +0600

    Problem07 is completed.

--- lab-03/src/Problem07.java
+++ lab-03/src/Problem07.java
@@ -26,6 +26,14 @@ public class Problem07 {
 
         System.out.print(" and the future day is ");
 
+        switch (futureDay) {
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
 
         }
     }
