e79723f0e065084e011fb9daa34cf6829724cdcb
Maha Abdul <abdul_@auca.kg>
2022-11-09 13:41:20 +0600

    Problem02 is accepted.

--- oj-final/src/Problem02.java
+++ oj-final/src/Problem02.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Problem02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int a = input.nextInt();
         input.nextLine();
 
