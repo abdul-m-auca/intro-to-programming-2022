61b9ac2a901c7bef9e9e8d340d786998ff3c18dc
Maha Abdul <abdul_@auca.kg>
2022-11-09 13:41:06 +0600

    Problem02 is not accepted.

---
+++ oj-final/src/Problem02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        input.nextLine();
+
+        for (int i = 1; i <= a; ++i) {
+
+            String word = input.nextLine();
+            if (i % 2 == 1) {
+                System.out.println(word);
+            }
+        }
+    }
+}
