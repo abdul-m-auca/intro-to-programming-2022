53344c4bf4c373722f40d21193659a26760ff026
Maha Abdul <abdul_@auca.kg>
2022-11-09 13:14:12 +0600

    Problem01 is accepted.

--- oj-final/src/Problem01.java
+++ oj-final/src/Problem01.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
+
 public class Problem01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        String a = input.next();
+
+        int letter = a.length() - 2;
+
+        System.out.print("h");
+        for (int i = 0; i < 2 * letter; ++i) {
+            System.out.print("e");
+        }
+        System.out.print("y");
 
     }
 }
