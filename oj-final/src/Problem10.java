8a4a5bcef2740e962dd18a9f198e635a4031aed2
Maha Abdul <abdul_@auca.kg>
2022-11-16 15:08:16 +0600

    Problem10 is solved.

--- oj-final/src/Problem10.java
+++ oj-final/src/Problem10.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Problem10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+        int sum = n;
+
+        for (int i = 0; i < k; i++) {
+            n += 10;
+            sum += n;
+        }
+        System.out.println(sum);
+    }
 }
