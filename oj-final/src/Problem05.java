6acd976c7c3320afd22ec9b01412bb0f24c05357
Maha Abdul <abdul_@auca.kg>
2022-11-23 12:32:42 +0600

    Problem05 is updated.

--- oj-final/src/Problem05.java
+++ oj-final/src/Problem05.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 public class Problem05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        int g = input.nextInt();
+        int t = input.nextInt();
+        int n = input.nextInt();
+        int allowedWeight = (g - t) * 9 / 10;
+        int overallExtraWeight = 0;
+        for (int i =0; i < n; i++ ){
+            int extraWeight = input.nextInt();
+            overallExtraWeight += extraWeight;
     }
 }
