2b66314d0e943ac8892bcdb6b584c28666a52f4c
Maha Abdul <abdul_@auca.kg>
2022-12-06 23:10:58 +0600

    Problem02 is finished.

--- lab-06/src/Problem02.java
+++ lab-06/src/Problem02.java
@@ -7,20 +7,26 @@ public class Problem02 {
         Scanner input = new Scanner(System.in);
         int year = readInt("year: ", "incorrect year", 1, 100000);
         int month = readInt("month: ", "incorrect month", 1, 12);
+        System.out.println("Number of days: " + getNumberOfDays(year, month));
     }
 
-    static int GetNumberOfDays(int year, int month) {
+    static int getNumberOfDays(int year, int month) {
         switch (month) {
             case 1:
-            case 2:
             case 3:
-            case 4:
             case 5:
-            case 6:
             case 7:
-            case 9:
+            case 8:
             case 10:
-            case 1p
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return isLeap(year) ? 29 : 28;
         }
     }
 
@@ -32,5 +38,10 @@ public class Problem02 {
             System.out.println(errorMessage);
             System.exit(1);
         }
+        return res;
+    }
+
+    static boolean isLeap(int year) {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
