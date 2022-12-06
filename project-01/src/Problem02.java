39fe84d7defda4a693b885c52a6a402ee5348caa
Maha Abdul <abdul_@auca.kg>
2022-12-06 19:31:21 +0600

    Working on Problem02 in Project01

--- project-01/src/Problem02.java
+++ project-01/src/Problem02.java
@@ -1,6 +1,13 @@
 import processing.core.*;
 
 public class Problem02 extends PApplet {
+    final float max = 180;
+    final float min = 50;
+
+    float start1 = min + (max - min) / 2;
+    float start2 = min + (max - min) / 2;
+    float num1 = 3;
+    float num2 = 3;
     float x;
     float y;
     float dx;
@@ -25,9 +32,22 @@ public class Problem02 extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
-        textSize(72);
+        textSize(msgSize);
+        fill(0, 255, 51);
+        text("Write once!!!", width / 2f, height / 2f);
         fill(255, 0, 0);
-        text("Hello, Processing!!!", width / 2f, height / 2f);
+        text("Java Slogan:", width / 2f, height / 4f);
+        fill(0, 0, 255);
+        text("run anywhere", width / 2f, height - height / 4f);
+
+        msgSize += dMsgSize;
+        if (msgSize < 20 || 100 <= msgSize) {
+            dMsgSize = -dMsgSize;
+        }
+        if (start1 > max) {
+            start1 = max;
+            num1 = -num1;
+        }
 
     }
 
