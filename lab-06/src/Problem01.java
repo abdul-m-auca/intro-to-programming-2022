9cd7dbd906c79baf7d7540ebbc9ae07844f97486
Maha Abdul <abdul_@auca.kg>
2022-12-06 22:50:10 +0600

    Problem01 is finished

--- lab-06/src/Problem01.java
+++ lab-06/src/Problem01.java
@@ -10,22 +10,28 @@ public class Problem01 {
         System.out.print("Coordinate of 1st point: ");
         int X1 = input.nextInt();
 
-        System.out.print("Coordinate of 2st point:");
+        System.out.print("Coordinate of 2st point: ");
         int X2 = input.nextInt();
 
-        int d1 = X1 - userX;
-        if (d1 < 1) {
-            d1 = -d1;
-        }
-        int d2 = X2 - userX;
-        if (d2 < 1) {
-            d2 = -d2;
+        //DRY do not repeat yourself
+
+        int d1 = myAbs(X1 - userX);
+        int d2 = myAbs(X2 - userX);
+
+        if (d1 < d2) {
+            System.out.println("1st is closer. Distance is " + d1);
+        } else if (d2 < d1) {
+            System.out.println("2nd is closer. Distance is " + d2);
+        } else {
+            System.out.println("Distance is the same " + d1);
         }
     }
 
+    //method
     static int myAbs(int n) {
         if (n < 0) {
             n = -n;
         }
+        return n;
     }
 }
