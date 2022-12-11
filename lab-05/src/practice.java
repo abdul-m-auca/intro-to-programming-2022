f1996576dd23ccc3ac4023ffa71dde437c5817b2
Maha Abdul <abdul_@auca.kg>
2022-12-11 17:52:05 +0600

    Problem09 is accepted.

--- lab-05/src/practice.java
+++ lab-05/src/practice.java
@@ -1,34 +1,23 @@
 import java.util.Scanner;
 
 public class practice {
-    public static final int TABLE_SIZE = 9;
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        //mul
-        System.out.println("multiplication table");
+        double sum = 0;
+        int nGrade = 0;
+        int grade = input.nextInt();
 
-        // draw the top
-        System.out.print(" ");
-        for (int i = 1; i <= TABLE_SIZE; i++) {
-            System.out.printf("%4d", i);
+        while (grade != 0) {
+            sum += grade;
+            ++nGrade;
+            grade = input.nextInt();
         }
-        System.out.println();
-
-        // draw the dash
-        for (int i = 0; i < TABLE_SIZE * 2 + 1; i++) {
-            System.out.printf(" -");
-        }
-        System.out.println();
-
-        // draw the row
-        for (int i = 1; i <= TABLE_SIZE; i++) {
-            System.out.printf("%d|", i);
-            for (int j = 1; j <= TABLE_SIZE; j++) {
-                System.out.printf("%4d", i * j);
-            }
-            System.out.println();
+        if (grade != 0) {
+            System.out.printf("%.1f%n", sum / nGrade);
+        } else {
+            System.out.println("No data");
         }
     }
+
 }
