6a35e5b03beda9c47f13c45acad879ce809802f2
Maha Abdul <abdul_@auca.kg>
2022-12-12 18:38:45 +0600

    Problem03 is finished.

--- lab-06/src/Practice.java
+++ lab-06/src/Practice.java
@@ -1,35 +1,49 @@
 import java.util.Scanner;
 
 public class Practice {
+    static Scanner inp = new Scanner(System.in);
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Your coordinate: ");
-        int userX = input.nextInt();
+        int year = readInt("year", "Incorrect number", 1, 100000);
+        int month = readInt("month", "Incorrect number", 1, 12);
 
-        System.out.println("Your first coordinate: ");
-        int X1 = input.nextInt();
+        System.out.println("Number of days" + GetNumberOfDays(year, month));
 
-        System.out.println("Your second coordinate: ");
-        int X2 = input.nextInt();
+    }
 
-        int d1 = myAbs(X1 - userX);
-        int d2 = myAbs(X2 - userX);
-
-
-        if (d1 < d2) {
-            System.out.println("Distance is closer to first. Distance is" + d1);
-        } else if (d2 < d1) {
-            System.out.println("Distance is closer to second. Distance is " + d2);
-        } else {
-            System.out.println("Distance is the same" + d1);
+    static int GetNumberOfDays(int year, int month) {
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return isLeap(year) ? 29 : 28;
         }
     }
 
-    static int myAbs(int n) {
-        if (n < 0) {
-            n = -n;
+    static int readInt(String prompt, String errorMessage, int minvalue, int maxvalue) {
+        System.out.print(prompt);
+        int res = inp.nextInt();
+        if (res < minvalue || res > maxvalue) {
+            System.out.println(errorMessage);
+            System.exit(1);
         }
-        return n;
+        return res;
+    }
+
+    static boolean isLeap(int year) {
+        return (year % 400 == 0 || year % 4 == 0 || year % 100 != 0);
     }
 }
--- lab-06/src/Problem03.java
+++ lab-06/src/Problem03.java
@@ -1,7 +1,8 @@
 import processing.core.*;
 
 public class Problem03 extends PApplet {
-    float angle = 0;
+
+    float angle;
 
     public void settings() {
         fullScreen();
@@ -9,25 +10,52 @@ public class Problem03 extends PApplet {
 
     public void setup() {
         strokeWeight(6);
+        angle = 0;
     }
 
     public void draw() {
-        background(0, 0, 0);
+        background(0);
 
-        float r = min(width, height) / 15f;
+        float r = min(width, height) / 10f;
 
-        translate(width / 2f, height / 2f);
+        drawStar(width / 2f, height / 2f, height / 10f, angle, 255, 0, 0);
+        drawStar(width / 4f, height / 4f, height / 15f, angle, 255, 255, 0);
+        drawStar(width / 4f * 3, height / 4f * 3, height / 15f, angle, 255, 255, 0);
+        drawStar(width / 4f * 3, height / 4f, height / 15f, angle, 255, 255, 0);
+        drawStar(width / 4f, height / 4f * 3, height / 15f, angle, 255, 255, 0);
+
+        angle += 0.05;
+
+    }
+
+    void drawStar(float xc, float yc, float r, float angle, float red, float green, float blue) {
+        pushMatrix();
+        translate(xc, yc);
         rotate(angle);
-        stroke(255, 0, 0);
+        stroke(red, green, blue);
+
+        // Draw the main lines
+        strokeWeight(3);
         line(-r, 0, r, 0);
+        line(0, -r, 0, r);
+        line(-r / 4f, r / 4f, r / 4f, -r / 4f);
+        line(r / 4f, r / 4f, -r / 4f, -r / 4f);
 
-        angle += 0.02f;
+        // Draw the outer lines
+        strokeWeight(2);
+        line(-r / 4f, r / 4f, 0, r);
+        line(0, r, r / 4f, r / 4f);
+        line(r / 4f, r / 4f, r, 0);
+        line(r, 0, r / 4f, -r / 4f);
+        line(r / 4f, -r / 4f, 0, -r);
+        line(0, -r, -r / 4f, -r / 4f);
+        line(-r / 4f, -r / 4f, -r, 0);
+        line(-r, 0, -r / 4f, r / 4f);
 
-
+        popMatrix();
     }
 
     public static void main(String[] args) {
-        PApplet.main("Problem03 ");
+        PApplet.main("Problem03");
     }
-
 }
